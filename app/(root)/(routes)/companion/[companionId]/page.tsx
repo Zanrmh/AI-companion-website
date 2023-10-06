aabce5196522126972a732c115a62e94cf69c521
Zanrmh <ramha1609@gmail.com>
2023-10-07 00:48:19 +0700

    create form

--- app/(root)/(routes)/companion/[companionId]/page.tsx
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -1,5 +1,24 @@
-const CompanionIdPage = () => {
-  return <div>Hello Companion ID!</div>;
+import prismadb from '@/lib/prismadb';
+import { CompanionForm } from './components/companion-form';
+
+interface CompanionIdPageProps {
+  params: {
+    companionId: string;
+  };
+}
+
+const CompanionIdPage = async ({ params }: CompanionIdPageProps) => {
+  // TODO: check subscriptions
+
+  const companion = await prismadb.companion.findUnique({
+    where: {
+      id: params.companionId,
+    },
+  });
+
+  const categories = await prismadb.category.findMany();
+
+  return <CompanionForm initialData={companion} categories={categories} />;
 };
 
 export default CompanionIdPage;
